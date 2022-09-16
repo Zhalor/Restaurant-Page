d6575033e3eec61675e77b67a300229c5306b61c
Jordan <uberbomb@yahoo.com>
2022-09-15 21:36:11 -0400

    Add functions to create menu buttons
    Added functions to create and append add item, edit, and delete buttons.
    Added styling to buttons and structured them in the DOM.

--- src/menu.js
+++ src/menu.js
@@ -1,3 +1,15 @@
+const arrAppitizers = [];
+const arrNoodles = [];
+const arrDrinks = [];
+
+const menuItem = class {
+  constructor(name, price, description) {
+    this.name = name;
+    this.price = price;
+    this.description = description;
+  }
+}
+
 const displayMenuPage = () => {
   const wrapper = document.getElementById('wrapper');
   const home = document.getElementById('home');
@@ -15,9 +27,9 @@ const displayMenuPage = () => {
   while(wrapper.firstChild !== null) {
     wrapper.removeChild(wrapper.lastChild);
   }
-  const pAppitizers = document.createElement('p');
-  const pNoodles = document.createElement('p');
-  const pDrinks = document.createElement('p');
+  const h2Appitizers = document.createElement('h2');
+  const h2Noodles = document.createElement('h2');
+  const h2Drinks = document.createElement('h2');
   const divAppetizerContainer = document.createElement('div');
   const divNoodleContainer = document.createElement('div');
   const divDrinkContainer = document.createElement('div');
@@ -29,14 +41,23 @@ const displayMenuPage = () => {
   divNoodleContainer.classList.add('noodles');
   divDrinkContainer.setAttribute('id', 'drinks');
   divDrinkContainer.classList.add('drinks');
+  h2Appitizers.classList.add('menu-header');
+  h2Noodles.classList.add('menu-header');
+  h2Drinks.classList.add('menu-header');
+  updateMenuBtn.setAttribute('id', 'update-menu');
 
-  pAppitizers.innerText = "Appitizers";
-  pNoodles.innerText = "Noodles";
-  pDrinks.innerText = "Drinks";
+  h2Appitizers.innerText = "Appitizers";
+  h2Noodles.innerText = "Noodles";
+  h2Drinks.innerText = "Drinks";
   updateMenuBtn.innerText = "Update Menu";
 
+  updateMenuBtn.addEventListener('click', () => {
+    createAddItemBtn();
+    createEditBtn();
+  });
+
   header.appendChild(updateMenuBtn);
-  wrapper.append(pAppitizers, divAppetizerContainer, pNoodles, divNoodleContainer, pDrinks,
+  wrapper.append(h2Appitizers, divAppetizerContainer, h2Noodles, divNoodleContainer, h2Drinks,
     divDrinkContainer);
 
   CreateAppitizerItem();
@@ -45,6 +66,50 @@ const displayMenuPage = () => {
   CreateNoodleItem();
   CreateNoodleItem();
   CreateDrinkItem();
+}
+
+const createAddItemBtn = () => {
+  if(document.getElementById('add-appitizer-btn') === null &&
+  document.getElementById('add-noodle-btn') === null &&
+  document.getElementById('add-drink-btn') === null){
+    const addAppetizerItem = document.createElement('button');
+    addAppetizerItem.classList.add('add-item');
+    addAppetizerItem.setAttribute('id', 'add-appitizer-btn');
+    addAppetizerItem.innerText = "Add Item";
+
+    const addNoodleItem = document.createElement('button');
+    addNoodleItem.classList.add('add-item');
+    addNoodleItem.setAttribute('id', 'add-noodle-btn');
+    addNoodleItem.innerText = "Add Item";
+
+    const addDrinkItem = document.createElement('button');
+    addDrinkItem.classList.add('add-item');
+    addDrinkItem.setAttribute('id', 'add-drink-btn');
+    addDrinkItem.innerText = "Add Item";
+    document.getElementById('drinks').appendChild(addDrinkItem);
+
+    let counter = 0;
+    const arr = [addAppetizerItem, addNoodleItem, addDrinkItem];
+    document.querySelectorAll('.menu-header').forEach(item => {
+      item.appendChild(arr[counter]);
+      counter++;
+    });
+  }
+}
+
+const createEditBtn = () => {
+  if(document.getElementsByClassName('edit-delete-btn').length === 0){
+    const menuItems = document.querySelectorAll('.menu-item');
+    menuItems.forEach(item => {
+    const editBtn = document.createElement('button');
+    const deleteBtn = document.createElement('button');
+    editBtn.classList.add('edit-delete-btn');
+    deleteBtn.classList.add('edit-delete-btn');
+    editBtn.innerText = "Edit";
+    deleteBtn.innerText = "Delete";
+    item.append(editBtn, deleteBtn);
+  });
+  }
 }
 
 const CreateAppitizerItem = () => {
@@ -88,5 +153,6 @@ const CreateDrinkItem = () => {
   document.getElementById('drinks').appendChild(divItemContainer);
   divItemContainer.append(pIItemTitle, pItemDescription);
 }
-export {displayMenuPage}
 
+
+export {displayMenuPage}
