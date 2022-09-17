ce18f0bcda14dbd82a14bf02b78709d289b00e22
Jordan <uberbomb@yahoo.com>
2022-09-17 11:35:57 -0400

    Create functions to add new menu items
    Created functions to add new menu items and append them to the appropriate
    array and menu section.

--- src/menu.js
+++ src/menu.js
@@ -1,12 +1,55 @@
-const arrAppitizers = [];
-const arrNoodles = [];
-const arrDrinks = [];
+const arrAppitizers = [{title: "Dumplings", price: "6", desc: "Tasty Dumplings."},
+                       {title: "Fried Rice", price: "3", desc: "Tasty Fried Rice."}];
+const arrNoodles = [{title: "Soba Noodles", price: "9", desc: "Tasty Soba."},
+                    {title: "Ramen Noodles", price: "9", desc: "Tasty Ramen."}];
+const arrDrinks = [{title: "Sake", price: "5", desc: "Tasty Sake."}]
 
-const menuItem = class {
-  constructor(name, price, description) {
-    this.name = name;
+let update = false;
+
+class MenuItem {
+  constructor(title, price, description) {
+    this.name = title;
     this.price = price;
     this.description = description;
+  }
+}
+
+class Appitizer extends MenuItem {
+  addAppitizer(appitizer) {
+    console.log(appitizer);
+    arrAppitizers.push(appitizer);
+    console.log(arrAppitizers);
+    console.log(arrAppitizers.indexOf(this));
+  }
+  removeAppitizer() {
+    arrAppitizers.splice(arrAppitizers.indexOf(this), 1);
+    console.log(arrAppitizers);
+  }
+}
+
+class Noodle extends MenuItem {
+  addNoodle(noodle) {
+    console.log(noodle);
+    arrNoodles.push(noodle);
+    console.log(arrNoodles);
+    console.log(arrNoodles.indexOf(this));
+  }
+  removeNoodle() {
+    arrNoodles.splice(arrNoodles.indexOf(this), 1);
+    console.log(arrNoodles);
+  }
+}
+
+class Drink extends MenuItem {
+  addDrink(drink) {
+    console.log(drink);
+    arrDrinks.push(drink);
+    console.log(arrDrinks);
+    console.log(arrDrinks.indexOf(this));
+  }
+  removeDrink() {
+    arrDrinks.splice(arrDrinks.indexOf(this), 1);
+    console.log(arrDrinks);
   }
 }
 
@@ -52,44 +95,61 @@ const displayMenuPage = () => {
   updateMenuBtn.innerText = "Update Menu";
 
   updateMenuBtn.addEventListener('click', () => {
-    createAddItemBtn();
-    createEditBtn();
+    if(update === false){
+      createAddItemBtn();
+      createEditBtn();
+      update = true;
+      updateMenuBtn.innerText = "Complete Update";
+    }
   });
 
   header.appendChild(updateMenuBtn);
   wrapper.append(h2Appitizers, divAppetizerContainer, h2Noodles, divNoodleContainer, h2Drinks,
     divDrinkContainer);
 
-  CreateAppitizerItem();
-  CreateAppitizerItem();
-  CreateNoodleItem();
-  CreateNoodleItem();
-  CreateNoodleItem();
-  CreateDrinkItem();
+  arrAppitizers.forEach(item => {
+    createAppitizer(item.title, item.price, item.desc);
+  });
+
+  arrNoodles.forEach(item => {
+    createNoodle(item.title, item.price, item.desc);
+  });
+
+  arrDrinks.forEach(item => {
+    createDrink(item.title, item.price, item.desc);
+  });
 }
 
 const createAddItemBtn = () => {
   if(document.getElementById('add-appitizer-btn') === null &&
   document.getElementById('add-noodle-btn') === null &&
   document.getElementById('add-drink-btn') === null){
-    const addAppetizerItem = document.createElement('button');
-    addAppetizerItem.classList.add('add-item');
-    addAppetizerItem.setAttribute('id', 'add-appitizer-btn');
-    addAppetizerItem.innerText = "Add Item";
+    const addAppitizerBtn = document.createElement('button');
+    addAppitizerBtn.classList.add('add-item-btn');
+    addAppitizerBtn.setAttribute('id', 'add-appitizer-btn');
+    addAppitizerBtn.innerText = "Add Item";
+    addAppitizerBtn.addEventListener('click', () => {
+      appitizerForm();
+    });
 
-    const addNoodleItem = document.createElement('button');
-    addNoodleItem.classList.add('add-item');
-    addNoodleItem.setAttribute('id', 'add-noodle-btn');
-    addNoodleItem.innerText = "Add Item";
+    const addNoodleBtn = document.createElement('button');
+    addNoodleBtn.classList.add('add-item-btn');
+    addNoodleBtn.setAttribute('id', 'add-noodle-btn');
+    addNoodleBtn.innerText = "Add Item";
+    addNoodleBtn.addEventListener('click', () => {
+      noodleForm();
+    });
 
-    const addDrinkItem = document.createElement('button');
-    addDrinkItem.classList.add('add-item');
-    addDrinkItem.setAttribute('id', 'add-drink-btn');
-    addDrinkItem.innerText = "Add Item";
-    document.getElementById('drinks').appendChild(addDrinkItem);
+    const addDrinkBtn = document.createElement('button');
+    addDrinkBtn.classList.add('add-item-btn');
+    addDrinkBtn.setAttribute('id', 'add-drink-btn');
+    addDrinkBtn.innerText = "Add Item";
+    addDrinkBtn.addEventListener('click', () => {
+      drinkForm();
+    });
 
     let counter = 0;
-    const arr = [addAppetizerItem, addNoodleItem, addDrinkItem];
+    const arr = [addAppitizerBtn, addNoodleBtn, addDrinkBtn];
     document.querySelectorAll('.menu-header').forEach(item => {
       item.appendChild(arr[counter]);
       counter++;
@@ -101,58 +161,156 @@ const createEditBtn = () => {
   if(document.getElementsByClassName('edit-delete-btn').length === 0){
     const menuItems = document.querySelectorAll('.menu-item');
     menuItems.forEach(item => {
+      const editBtn = document.createElement('button');
+      const deleteBtn = document.createElement('button');
+      editBtn.classList.add('edit-delete-btn');
+      deleteBtn.classList.add('edit-delete-btn');
+      editBtn.innerText = "Edit";
+      deleteBtn.innerText = "Delete";
+      item.append(editBtn, deleteBtn);
+  });
+  }
+}
+
+const appitizerForm = () => {
+  const divItemContainer = document.createElement('div');
+  const inputItemTitle = document.createElement('input');
+  const inputItemDescription = document.createElement('input');
+  const inputItemPrice = document.createElement('input');
+  const addItembtn = document.createElement('button');
+  inputItemTitle.type = "text";
+  inputItemPrice.type = "number";
+  inputItemDescription.type = "text";
+  addItembtn.innerText = "Add";
+
+  addItembtn.addEventListener('click', () => {
+    createAndAddApp(inputItemTitle.value, inputItemPrice.value, inputItemDescription.value);
+    divItemContainer.remove();
+  });
+
+  divItemContainer.classList.add('menu-item');
+
+  document.getElementById('appitizers').appendChild(divItemContainer);
+  divItemContainer.append(inputItemTitle, inputItemPrice, inputItemDescription, addItembtn);
+}
+
+const createAndAddApp = (title, price, desc) => {
+  const newAppitizer = new Appitizer(title, price, desc);
+  newAppitizer.addAppitizer(newAppitizer);
+  createAppitizer(title, price, desc);
+}
+
+const createAppitizer = (title, price, desc) => {
+  const divItemContainer = document.createElement('div');
+  const pItemTitle = document.createElement('p');
+  const pItemDescription = document.createElement('p');
+
+  divItemContainer.classList.add('menu-item');
+
+  pItemTitle.innerText = `${title}...............$${price}`;
+  pItemDescription.innerText = desc;
+
+  document.getElementById('appitizers').appendChild(divItemContainer);
+  divItemContainer.append(pItemTitle, pItemDescription);
+  if(update === true) {
     const editBtn = document.createElement('button');
     const deleteBtn = document.createElement('button');
     editBtn.classList.add('edit-delete-btn');
     deleteBtn.classList.add('edit-delete-btn');
     editBtn.innerText = "Edit";
     deleteBtn.innerText = "Delete";
-    item.append(editBtn, deleteBtn);
-  });
+    divItemContainer.append(editBtn, deleteBtn);
   }
+
+  console.log(arrAppitizers);
 }
 
-const CreateAppitizerItem = () => {
+const noodleForm = () => {
   const divItemContainer = document.createElement('div');
-  const pIItemTitle = document.createElement('p');
-  const pItemDescription = document.createElement('p');
+  const inputItemTitle = document.createElement('input');
+  const inputItemDescription = document.createElement('input');
+  const inputItemPrice = document.createElement('input');
+  const addItembtn = document.createElement('button');
+  inputItemTitle.type = "text";
+  inputItemPrice.type = "number";
+  inputItemDescription.type = "text";
+  addItembtn.innerText = "Add";
+
+  addItembtn.addEventListener('click', () => {
+    createAndAddNoodle(inputItemTitle.value, inputItemPrice.value, inputItemDescription.value);
+    divItemContainer.remove();
+  });
 
   divItemContainer.classList.add('menu-item');
-
-  pIItemTitle.innerText = "Dumplings...............$6";
-  pItemDescription.innerText = "Delicious noodles that make you feel like you're at moms house."
-
-  document.getElementById('appitizers').appendChild(divItemContainer);
-  divItemContainer.append(pIItemTitle, pItemDescription);
-}
-
-const CreateNoodleItem = () => {
-  const divItemContainer = document.createElement('div');
-  const pIItemTitle = document.createElement('p');
-  const pItemDescription = document.createElement('p');
-
-  divItemContainer.classList.add('menu-item');
-
-  pIItemTitle.innerText = "Noodles...............$10";
-  pItemDescription.innerText = "Delicious noodles that make you feel like you're at moms house."
 
   document.getElementById('noodles').appendChild(divItemContainer);
-  divItemContainer.append(pIItemTitle, pItemDescription);
+  divItemContainer.append(inputItemTitle, inputItemPrice, inputItemDescription, addItembtn);
 }
 
-const CreateDrinkItem = () => {
+const createAndAddNoodle = (title, price, desc) => {
+  const newNoodle = new Noodle(title, price, desc);
+  newNoodle.addNoodle(newNoodle);
+  createNoodle(title, price, desc);
+}
+
+const createNoodle = (title, price, desc) => {
   const divItemContainer = document.createElement('div');
-  const pIItemTitle = document.createElement('p');
+  const pItemTitle = document.createElement('p');
   const pItemDescription = document.createElement('p');
 
   divItemContainer.classList.add('menu-item');
 
-  pIItemTitle.innerText = "Chai Tea...............$3";
-  pItemDescription.innerText = "Delicious noodles that make you feel like you're at moms house."
+  pItemTitle.innerText = `${title}...............$${price}`;
+  pItemDescription.innerText = desc;
 
-  document.getElementById('drinks').appendChild(divItemContainer);
-  divItemContainer.append(pIItemTitle, pItemDescription);
+  document.getElementById('noodles').appendChild(divItemContainer);
+  divItemContainer.append(pItemTitle, pItemDescription);
+
+  console.log(arrNoodles);
 }
 
+const drinkForm = () => {
+  const divItemContainer = document.createElement('div');
+  const inputItemTitle = document.createElement('input');
+  const inputItemDescription = document.createElement('input');
+  const inputItemPrice = document.createElement('input');
+  const addItembtn = document.createElement('button');
+  inputItemTitle.type = "text";
+  inputItemPrice.type = "number";
+  inputItemDescription.type = "text";
+  addItembtn.innerText = "Add";
+
+  addItembtn.addEventListener('click', () => {
+    createAndAddDrink(inputItemTitle.value, inputItemPrice.value, inputItemDescription.value);
+    divItemContainer.remove();
+  });
+
+  divItemContainer.classList.add('menu-item');
+
+  document.getElementById('drinks').appendChild(divItemContainer);
+  divItemContainer.append(inputItemTitle, inputItemPrice, inputItemDescription, addItembtn);
+}
+
+const createAndAddDrink = (title, price, desc) => {
+  const newDrink = new Drink(title, price, desc);
+  newDrink.addDrink(newDrink);
+  createDrink(title, price, desc);
+}
+
+const createDrink = (title, price, desc) => {
+  const divItemContainer = document.createElement('div');
+  const pItemTitle = document.createElement('p');
+  const pItemDescription = document.createElement('p');
+
+  divItemContainer.classList.add('menu-item');
+
+  pItemTitle.innerText = `${title}...............$${price}`;
+  pItemDescription.innerText = desc;
+
+  document.getElementById('drinks').appendChild(divItemContainer);
+  divItemContainer.append(pItemTitle, pItemDescription);
+
+  console.log(arrDrinks);
+}
 
 export {displayMenuPage}
